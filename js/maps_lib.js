09c8f71aba2fc2bf2093ff79c406639297243108
Pam Broviak <pwg@publicworksgroup.com>
2016-09-05 06:55:41 -0500

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -9,16 +9,16 @@
         this.searchRadius = options.searchRadius || 805; //in meters ~ 1/2 mile
 
         // the encrypted Table ID of your Fusion Table (found under File => About)
-        this.fusionTableId = options.fusionTableId || "",
+        this.fusionTableId = options.fusionTableId || "1MTXAMdbX5Boo6OcGFRh2SgwAfifExBbOhMMFjyeS",
 
         // Found at https://console.developers.google.com/
         // Important! this key is for demonstration purposes. please register your own.
-        this.googleApiKey = options.googleApiKey || "",
+        this.googleApiKey = options.googleApiKey || "AIzaSyBVjrNDL2VUFrgrnjzihiT25TFz6t2JZsU",
         
         // name of the location column in your Fusion Table.
         // NOTE: if your location column name has spaces in it, surround it with single quotes
         // example: locationColumn:     "'my location'",
-        this.locationColumn = options.locationColumn || "geometry";
+        this.locationColumn = options.locationColumn || "Address";
         
         // appends to all address searches if not present
         this.locationScope = options.locationScope || "";
